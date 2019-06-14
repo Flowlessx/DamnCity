efe2259dc9642c69aa7a621fb9b954e95ed6d5c1
DH <davidhoogstraaten@gmail.com>
2019-06-14 09:08:10 +0000

    heroku update

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+var express = require(heroku);
+var port = process.env.PORT || 3000;
+var app = express();
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'World'}));
 });
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
 });
-
