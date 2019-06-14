1660606af8c5ace3bbc525991d34b61484ff2cc8
DH <davidhoogstraaten@gmail.com>
2019-06-14 08:29:29 +0000

    Update server

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const message = 'Hello World';
-console.log(message, 'from Gitpod!');
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
